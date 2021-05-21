f0aad21b4309e38dca9565e313d2ce450a9cf8fa
Florian Rebaudo <frebaudo@witekio.com>
2021-05-20 19:51:00 -0700

    recipes-app: run-1920-1080: update IP's addresses and V4L2 connector, crtc and plane ID

--- recipes-app/run-1920-1080/files/run_1920_1080.sh
+++ recipes-app/run-1920-1080/files/run_1920_1080.sh
@@ -7,15 +7,15 @@ OUTPUT_RESOLUTION=${OUTPUT_W}x${OUTPUT_H}
 
 media-ctl -d /dev/media0 -V "'ap1302.4-003c':2 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
 
-media-ctl -d /dev/media0 -V "'a0020000.mipi_csi2_rx_subsystem':0 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
-media-ctl -d /dev/media0 -V "'a0020000.mipi_csi2_rx_subsystem':1 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
+media-ctl -d /dev/media0 -V "'b0000000.mipi_csi2_rx_subsystem':0 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
+media-ctl -d /dev/media0 -V "'b0000000.mipi_csi2_rx_subsystem':1 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
 
-media-ctl -d /dev/media0 -V "'a0080000.v_proc_ss':0 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
-media-ctl -d /dev/media0 -V "'a0080000.v_proc_ss':1 [fmt:UYVY8_1X16/$OUTPUT_RESOLUTION field:none]"
+media-ctl -d /dev/media0 -V "'b0040000.v_proc_ss':0 [fmt:UYVY8_1X16/$INPUT_RESOLUTION field:none]"
+media-ctl -d /dev/media0 -V "'b0040000.v_proc_ss':1 [fmt:UYVY8_1X16/$OUTPUT_RESOLUTION field:none]"
 
-modetest -M xlnx -s 42:$OUTPUT_RESOLUTION@RG16 -P 38@40:$OUTPUT_RESOLUTION@YUYV -w 39:alpha:0 &
+modetest -M xlnx -s 43:$OUTPUT_RESOLUTION@RG16 -P 39@41:$OUTPUT_RESOLUTION@YUYV -w 40:alpha:0 &
 sleep 1
 
 gst-launch-1.0 v4l2src device=/dev/video0 io-mode="dmabuf" ! "video/x-raw, width=$OUTPUT_W, height=$OUTPUT_H, format=YUY2, framerate=60/1" \
 	        ! videoconvert \
-	        ! kmssink plane-id=38 bus-id=fd4a0000.zynqmp-display render-rectangle="<0,0,$OUTPUT_W,$OUTPUT_H> fullscreen-overlay=true sync=false" -v
+	        ! kmssink plane-id=39 bus-id=fd4a0000.zynqmp-display render-rectangle="<0,0,$OUTPUT_W,$OUTPUT_H> fullscreen-overlay=true sync=false" -v
